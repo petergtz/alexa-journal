54173d7b8b06bad35fa2d331f2f375d60e20500c
Peter Goetz <pego@amazon.com>
2021-07-09 22:28:59 +0200

    Build cross-platform to enable build & deploy from Mac

--- scripts/deploy-code.sh
+++ scripts/deploy-code.sh
@@ -6,7 +6,7 @@ function_name=AlexaJournal
 cd $(dirname $0)/..
 
 pushd cmd/skill
-    go build -o main
+    GOOS=linux go build -o main
 
     rm -f $zip_file
     zip $zip_file main
@@ -23,4 +23,3 @@ aws --region us-east-1 lambda update-function-code --function-name $function_nam
 aws --region eu-west-1 lambda update-function-code --function-name $function_name --s3-bucket alexa-golang-skills-eu-west-1 --s3-key $zip_file &
 aws --region ap-northeast-1 lambda update-function-code --function-name $function_name --s3-bucket alexa-golang-skills-ap-northeast-1 --s3-key $zip_file &
 wait
-
