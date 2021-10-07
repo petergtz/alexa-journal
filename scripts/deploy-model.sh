9ea0a267472281b7bfbaffd4cc0b2cdd65ea8dc1
Peter Goetz <pego@amazon.com>
2021-10-07 22:28:35 +0200

    Update deploy-model script

--- scripts/deploy-model.sh
+++ scripts/deploy-model.sh
@@ -1,16 +1,5 @@
 #!/bin/bash -ex
 
-ask diff  --target model
-
-while true; do
-    read -p "Deploy changes? " yn
-    case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit 1;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-ask deploy  --target model
+ask deploy
 
 rm -rf hooks
